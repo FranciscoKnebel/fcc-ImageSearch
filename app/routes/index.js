ddc32caa8170d530e82792e449f13245f9ef1c43
KyoZeus <franciscopaivaknebel@gmail.com>
2016-03-07 01:50:45 +0000

    Added basic google search for images

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,18 +1,58 @@
-var express = require("express");
+var dotenv = require("dotenv");
+var request = require("request");
 
 module.exports = function(app, dirname) {
     
     app.get('/', function(req, res) {
-            res.sendFile(dirname + '/client/views/index.html');
-        });
+        res.sendFile(dirname + '/client/views/index.html');
+    });
         
     app.get('/LICENSE/', function(req, res) {
-            res.sendFile(dirname + '/LICENSE.md');
+        res.sendFile(dirname + '/LICENSE.md');
+    });
+        
+    app.get('/latest', function(req, res) {
+        //Get latest query terms from DB.
+        res.send("Latest.");
+    })
+    
+    app.get('/search', function(req, res) {
+        res.sendFile(dirname + '/client/views/index.html'); 
+    });
+    
+    app.get('/search/:query', function(req, res) {
+        var url = 'https://www.googleapis.com/customsearch/v1?key=' + process.env.GOOGLE_API + '&cx=' + process.env.CSE_ID;
+        var query = '&q=' + req.params.query;
+        var searchType = '&searchType=image&alt=json';
+       
+        request(url + query + searchType, function(error, response, body) {
+            if(!error && response.statusCode == 200) {
+                var obj = JSON.parse(body);
+                var arr = [];
+                
+                for (var i = 0; i < obj.items.length; i++) {
+                    arr.push(queryItem(obj.items[i]));
+                }
+                
+                res.contentType('application/json');
+                res.send(arr);
+            }
+            else
+                res.send("Failed! Try again later, might have hit Google API quota.");
         });
-    
-    //404
-    app.get('/*', function(req, res) {
-            res.sendFile(dirname + '/client/views/404.html');
-        })
-    
+    });
 };
+
+
+function queryItem(element) {
+    var obj = {
+        url: element.link,
+        title: element.title,
+        snippet: element.snippet,
+        mime: element.mime,
+        thumbnail: element.image.thumbnailLink,
+        image: element.image.contextLink
+    }
+    
+    return obj;
+}
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var mongoose = require("mongoose");
-var dotenv = require("dotenv");
+var dotenv = require("dotenv").load();
 var routes = require("./app/routes/index.js");
 var api = require("./app/api.js");
 
@@ -15,6 +15,11 @@ app.use('/css', express.static(__dirname + '/client/css'));
 
 routes(app, __dirname);
 api(app, db, __dirname);
+
+//404
+app.get('/*', function(req, res) {
+    res.sendFile(__dirname + '/client/views/404.html');
+});
 
 app.listen(port,  function () {
 	console.log('Listening on port ' + port + '...');
